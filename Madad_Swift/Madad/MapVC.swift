ccf683ef26c52bbc8092d6f0a51d93267b8e2278
Hagar Sabet <hh1004136@qu.edu.qa>
2019-11-04 02:28:28 +0300

    func of loading videos from server
    Prepared the code to get the videos from the server

--- Madad_Swift/Madad/MapVC.swift
+++ Madad_Swift/Madad/MapVC.swift
@@ -1,6 +1,6 @@
 //
 //  MapVC.swift
-//  LookOut
+//  Madad
 //
 //  Created by Hagar Hussein on 10/25/19.
 //  Copyright © 2019 Qatar. All rights reserved.
@@ -38,6 +38,8 @@ class MapVC: UIViewController, MKMapViewDelegate, VideoMapViewDelegate {
         checkLocationAuthorizationStatus()
            
      initializeVideos()
+        
+       // loadVideos()
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -97,22 +99,13 @@ class MapVC: UIViewController, MKMapViewDelegate, VideoMapViewDelegate {
     
     
     func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
-       /* let visibleRegion = MKCoordinateRegion(center: userLocation.coordinate, latitudinalMeters: 5000, longitudinalMeters: 5000)
-        let addPin = MKPointAnnotation()
-        addPin.coordinate = CLLocationCoordinate2D(latitude: self.mapView.userLocation.coordinate.latitude, longitude: self.mapView.userLocation.coordinate.longitude)
-        addPin.title = "You are here"
-        mapView.addAnnotation(annotation)
-        self.mapView.setRegion(self.mapView.regionThatFits(visibleRegion), animated: true)*/
-        
-        
-        
-        //if let location = locations.last{
+     
             let center = CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude)
             let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
         let addPin = MKPointAnnotation()
         addPin.coordinate = CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude)
         addPin.title = "You are here"
-        mapView.addAnnotation(annotation)
+        mapView.addAnnotation(addPin)
             self.mapView.setRegion(region, animated: true)
     }
     
@@ -143,54 +136,35 @@ class MapVC: UIViewController, MKMapViewDelegate, VideoMapViewDelegate {
         self.videos.removeAll(keepingCapacity: false)
         self.mapView.removeAnnotations(self.mapView.annotations)
         
-        var filename = "IneedHelp"
+        var filename = "3"
         print(filename)
         
         guard let path1 = Bundle.main.path(forResource: filename, ofType:"mp4") else {
             debugPrint("video.mp4 not found")
             return
         }
-        ///// videos
-        //var path = "http://\(localhost):5000/videos/0.mp4"
-        var location = VideoNotification(Path: path1, Lat: 25.322508, Lng: 51.528355)
+       
+        var location = VideoNotification(Path: path1, Lat: 25.3000, Lng: 51.5100)
        
         videos.append( location )
         
         
-        filename = "IneedHelp"
+        
+        filename = "4"
         print(filename)
         
-        guard let path2 = Bundle.main.path(forResource: filename, ofType:"mp4") else {
-            debugPrint("video.mp4 not found")
-            return
-        }
-         location = VideoNotification(Path: path2, Lat: 25.2608759, Lng: 51.616025)
-        videos.append( location )
-        
-        
-        filename = "Pleaseleavethebuildingimmediately"
-        print(filename)
-        
-        guard let path3 = Bundle.main.path(forResource: filename, ofType:"mp4") else {
-            debugPrint("video.mp4 not found")
-            return
-        }
-        
-       location = VideoNotification(Path: path3, Lat: 25.2543318, Lng: 51.5659578)
-        videos.append( location )
-        
-        filename = "PleaseSitDown"
-        print(filename)
-        
-        guard let path4 = Bundle.main.path(forResource: filename, ofType:"mp4") else {
+        guard let path11 = Bundle.main.path(forResource: filename, ofType:"mp4") else {
             debugPrint("video.mp4 not found")
             return
         }
         
         
         
-        location = VideoNotification(Path: path4, Lat: 25.3220815, Lng: 51.4395895)
+        location = VideoNotification(Path: path11, Lat: 25.2950, Lng: 51.5192)
         videos.append( location )
+        
+        
+        
         
         filename = "Thereisabombthreat"
         print(filename)
@@ -201,9 +175,46 @@ class MapVC: UIViewController, MKMapViewDelegate, VideoMapViewDelegate {
         }
         
 
-        location = VideoNotification(Path: path5, Lat: 25.3133482, Lng: 51.5214587)
+        location = VideoNotification(Path: path5, Lat: 25.3136, Lng: 51.5214)
         videos.append( location )
         
+        
+        filename = "9"
+        print(filename)
+        
+        guard let path6 = Bundle.main.path(forResource: filename, ofType:"mp4") else {
+            debugPrint("video.mp4 not found")
+            return
+        }
+        
+        location = VideoNotification(Path: path6, Lat: 25.3137, Lng: 51.5200)
+        videos.append( location )
+        
+        
+        filename = "Pleaseleavethebuildingimmediately"
+        print(filename)
+        
+        guard let path7 = Bundle.main.path(forResource: filename, ofType:"mp4") else {
+            debugPrint("video.mp4 not found")
+            return
+        }
+        
+        location = VideoNotification(Path: path7, Lat: 25.3131655, Lng: 51.5217304)
+        videos.append( location )
+        
+        
+        filename = "5"
+        print(filename)
+        
+        guard let path8 = Bundle.main.path(forResource: filename, ofType:"mp4") else {
+            debugPrint("video.mp4 not found")
+            return
+        }
+        
+        location = VideoNotification(Path: path8, Lat: 25.3131000, Lng: 51.5217300)
+        videos.append( location )
+        
+      
         
         
         
@@ -211,15 +222,82 @@ class MapVC: UIViewController, MKMapViewDelegate, VideoMapViewDelegate {
        
     }
     
+    
+    // func of loading videos from server
+    public func loadVideos() {
+        
+        // need to modify url
+        let url = URL(string: "http://\(serverhost):5000/videos")!
+        
+        
+        var request = URLRequest(url: url)
+        
+        // declare method of passing information to server
+        request.httpMethod = "GET"
+        
+        // launch session
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            
+            // no error
+            if error == nil {
+                
+                do {
+                    
+                    let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as? NSDictionary
+                    
+                    
+                    self.videos.removeAll(keepingCapacity: false)
+                    
+                    self.mapView.removeAnnotations(self.mapView.annotations)
+                    
+                    guard let parseJSON = json else {
+                        print("Error while parsing")
+                        return
+                    }
+                    
+                    //////////// json file  {"videos":[RowDataPacket{"path":"","lat":"","lng":""}]}
+                    guard let videos = parseJSON["videos"]  as? [AnyObject] else {
+                        print("Error while parseJSONing")
+                        return
+                    }
+                    
+                    self.videos = videos
+                   
+                    for i in 0 ..< self.videos.count {
+                        
+                        let  Path = self.videos[i]["path"] as? String
+                        let  Lat = self.videos[i]["lat"] as! Double;
+                        let   Lng = self.videos[i]["lng"] as!  Double;
+                      
+                        
+                        
+                        if let  Path = Path
+                            {
+                            
+                            let currentPost =  VideoNotification(Path: Path, Lat: Lat, Lng: Lng)
+                            
+                            
+                            self.videoMap.append( currentPost )
+                            
+                        }
+                        
+                        
+                        self.mapView.addAnnotations(self.videoMap)
+                        
+                        
+                    }
+                    
+                    
+                } catch {
+                    print("error:\(error)")
+                }
+                
+            }
+            
+            }.resume()
+        
+    }
+    
+    
    
 }
-
-
-
-
-
-
-
-
-
-
